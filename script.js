41e8d0fac3aa6541da353bd0fdd4635af28e5819
linyunliu <linyunliu574@gmail.com>
2023-11-02 22:26:28 -0700

    Labs updates 1.1

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ function toggleNav() {
         nav.className = "topnav";
     }
 }
+
+function removeSomeNav(){
+    const topnav = document.getElementById('topnav');
+    const links = topnav.getElementsByTagName('a');
+    for (let i = links.length-1; i >= 0; i--) {
+        // Check if the href attribute is empty
+        if (links[i].getAttribute('href') === "") {
+            // Remove the <a> element if it matches the criteria
+            topnav.removeChild(links[i]);
+        }
+    }
+
+}
