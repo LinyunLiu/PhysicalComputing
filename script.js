3ecd46c2ea6c7626e92bfd503a5f7f819be2c1d0
linyunliu <linyunliu574@gmail.com>
2023-11-28 15:35:19 -0800

    senior device commit 1.0

--- script.js
+++ script.js
@@ -8,15 +8,15 @@ function toggleNav() {
 }
 
 function removeSomeNav(){
-    const topnav = document.getElementById('topnav');
-    const links = topnav.getElementsByTagName('a');
-    for (let i = links.length-1; i >= 0; i--) {
-        // Check if the href attribute is empty
-        if (links[i].getAttribute('href') === "") {
-            // Remove the <a> element if it matches the criteria
-            topnav.removeChild(links[i]);
-        }
-    }
+    // const topnav = document.getElementById('topnav');
+    // const links = topnav.getElementsByTagName('a');
+    // for (let i = links.length-1; i >= 0; i--) {
+    //     // Check if the href attribute is empty
+    //     if (links[i].getAttribute('href') === "") {
+    //         // Remove the <a> element if it matches the criteria
+    //         topnav.removeChild(links[i]);
+    //     }
+    // }
     getRidOfHTML()
 }
 
